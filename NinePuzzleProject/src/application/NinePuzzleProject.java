366c23a68bbc41c96602caac055c56d3687025c5
Ishmael Moreno <77425666+IshmaelMorenoH@users.noreply.github.com>
2024-07-14 17:58:40 -0400

    Fixed Typo.

--- NinePuzzleProject/src/application/NinePuzzleProject.java
+++ NinePuzzleProject/src/application/NinePuzzleProject.java
@@ -1,6 +1,6 @@
 /*
  * This is an interactive puzzle game that has 9 available spaces with numbers 1-8 and an empty
- * space to shift the numbers around. The goal of the game is to solve the puzzle by shifting the numbers around
+ * space to shuffle the numbers around. The goal of the game is to solve the puzzle by sliding the numbers around
  * until they are in order with a blank space at the end.
  */
 package application;
